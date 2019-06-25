c6c2f26a0c807803ec7e1913f951e2619b2f4af2
satokadumasa <sato.kadumasa@gmail.com>
2019-06-25 21:02:50 +0900

    公開環境で起動するかどうか（１）

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,76 +1,31 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
-  # Do not eager load code on boot.
   config.eager_load = false
-
-  # Show full error reports.
   config.consider_all_requests_local = true
-
-  # 静的なファイルをRailsから返す
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
-  # Enable/disable caching. By default caching is disabled.
-  # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
-
-  # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
-
-  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-
-  # Print deprecation notices to the Rails logger.
+  # config.action_mailer.perform_caching = false
+  config.log_formatter = ::Logger::Formatter.new
   config.active_support.deprecation = :log
-
-  # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
-  # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
   config.assets.debug = true
-
-  # Suppress logger output for asset requests.
   config.assets.quiet = true
-
-  # Action Cable
-  # config.action_cable.allowed_request_origins = [ 'http://hesper.example.com' ]
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://' + ENV['BAMBOO_HOST'] + '/cable'
   # config.action_cable.allowed_request_origins = [ 'http://' + ENV['BAMBOO_HOST'] , /http:\/\/hesper.example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'http://' + ENV['BAMBOO_HOST'] ]
+  config.action_cable.allowed_request_origins = [ 'https://' + ENV['BAMBOO_HOST'], 'http://' + ENV['BAMBOO_HOST'] ]
   config.web_console.whitelisted_ips = '127.0.0.1'
-
-  # Websocket
-  # onfig.middleware.delete Rack::Lock
-
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'hesper.example.com', :protocol => 'http' }
   config.action_mailer.smtp_settings   = {

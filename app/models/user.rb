e5b77407b0f6e17dcb30e2de740e4a5e9075e0e6
satokadumasa <sato.kadumasa@gmail.com>
2019-09-04 22:08:36 +0900

    devise_token_auth 追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable
-            include DeviseTokenAuth::Concerns::User
+  include DeviseTokenAuth::Concerns::User
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
